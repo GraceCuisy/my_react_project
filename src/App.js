a33baefcd2b03894257c2f71f4d5332dea05c3fb
Grace Cui <17831135193@163.com>
2020-04-28 21:50:57 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
-import {Button} from 'antd'
+import {Route,Switch,Redirect} from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Admin from "./pages/Admin/Admin"
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        cuishuyuan fighting...
-        <Button type='primary'>点我</Button>
-      </div>
+        <Switch>
+          <Route path="/login" component={Login}/>
+          <Route path="/admin" component={Admin}/>
+          <Redirect to="/login"/>
+        </Switch>
     )
   }
 }
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import { Form, Input, Button} from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+import Logo from "./images/logo.png";
+import "./css/Login.less";
+
+const {Item}=Form;
+
+export default class Login extends Component {
+  onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
+  /*用户名/密码的的合法性要求
+    1). 必须输入
+    2). 必须大于等于4位
+    3). 必须小于等于12位
+    4). 必须是英文、数字或下划线组成 */
+    // value是antd是底层帮你搜集的用户输入的值,但是默认是undefined
+    psdValidator=(_,value='') => {
+      const errorArr=[];
+      if(!value.trim()) return Promise.reject('必须输入');
+      if(value.length<4) errorArr.push('必须大于等于4位');
+      if(value.length>12) errorArr.push('必须小于等于12位');
+      if(!(/^\w+$/).test()) errorArr.push('必须是英文、数字或下划线组成');
+      if(errorArr.length>0) return Promise.reject(errorArr);
+      else return Promise.resolve();
+    }
+  render() {
+    return (
+      <div className='login'>
+        <header>
+          <img src={Logo} alt="indexlogo"/>
+          <h1>商品管理系统</h1>
+        </header>
+        <section>
+          <span>用户登录</span>
+          <Form
+            className="login-form"
+            onFinish={this.onFinish}
+          >
+            <Item
+              name="username"
+              rules={[
+                {required:true,message:'用户名必须输入'},
+                {min:4,message:'必须大于等于4位'},
+                {max:12,message:'不能超过12位'},
+                {pattern:(/^\w+$/),message:'必须是英文、数字或下划线组成'}
+              ]}
+            >
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+            </Item>
+            <Item
+              name="password"
+              rules={[
+                {validator:this.psdValidator}
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="密码"
+              />
+            </Item>
+            <Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                登录
+              </Button>
+            </Item>
+          </Form>
+        </section>
+      </div>
+    )
+  }
+}
